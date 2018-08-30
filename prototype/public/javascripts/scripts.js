3af2a270129b79730296b304198407108e25f2ca
Greg1586 <gskinner@thoughtworks.com>
2018-08-30 14:49:06 +1000

    fucks sake

---
+++ prototype/public/javascripts/scripts.js
@@ -0,0 +1,141 @@
+$( "#date" ).datepicker();
+
+$( "#accordion" ).accordion();
+
+var availableTags = [
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure",
+    "COBOL",
+    "ColdFusion",
+    "Erlang",
+    "Fortran",
+    "Groovy",
+    "Haskell",
+    "Java",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Scala",
+    "Scheme"
+];
+$( "#autocomplete" ).autocomplete({
+    source: availableTags
+});
+
+
+
+$( "#button" ).button();
+$( "#button-icon" ).button({
+    icon: "ui-icon-gear",
+    showLabel: false
+});
+
+
+
+$( "#radioset" ).buttonset();
+
+
+
+$( "#controlgroup" ).controlgroup();
+
+
+
+$( "#tabs" ).tabs();
+
+
+
+$( "#dialog" ).dialog({
+    autoOpen: false,
+    width: 400,
+    buttons: [
+        {
+            text: "Ok",
+            click: function() {
+                $( this ).dialog( "close" );
+            }
+        },
+        {
+            text: "Cancel",
+            click: function() {
+                $( this ).dialog( "close" );
+            }
+        }
+    ]
+});
+
+// Link to open the dialog
+$( "#dialog-link" ).click(function( event ) {
+    $( "#dialog" ).dialog( "open" );
+    event.preventDefault();
+});
+
+
+
+$( "#datepicker" ).datepicker({
+    inline: true
+});
+
+
+
+$( "#slider" ).slider({
+    range: true,
+    values: [ 17, 67 ]
+});
+
+
+
+$( "#progressbar" ).progressbar({
+    value: 20
+});
+
+
+
+$( "#spinner" ).spinner();
+
+
+
+$( "#menu" ).menu();
+
+
+
+$( "#tooltip" ).tooltip();
+
+
+
+$( "#selectmenu" ).selectmenu();
+
+
+// Hover states on the static widgets
+$( "#dialog-link, #icons li" ).hover(
+    function() {
+        $( this ).addClass( "ui-state-hover" );
+    },
+    function() {
+        $( this ).removeClass( "ui-state-hover" );
+    }
+);
+
+$(document).ready(function(){
+    $(".m_switch_check:checkbox").mSwitch({
+        onRender:function(elem){
+            // allows to apply a first state to the rendering of the CHECKBOX
+        },
+        onRendered:function(elem){
+            // exec your logic when the render is complete
+        },
+        onTurnOn:function(elem){
+            // exec your logic when the switch is activated
+        },
+        onTurnOff:function(elem){
+            // exec your logic when the switch is deactivated
+        }
+    });
+});
