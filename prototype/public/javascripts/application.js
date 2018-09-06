f79b723e61b849894b62acfab94ea7832a0fe3c9
Greg1586 <gskinner@thoughtworks.com>
2018-09-06 14:43:46 +1000

    lord of the javascrip

--- prototype/public/javascripts/application.js
+++ prototype/public/javascripts/application.js
@@ -1,209 +1,69 @@
 $(document).ready(function() {
+//togglers
 
-//accordion
-  $(".accordion-header").click( function() {
-    $(this).parent().toggleClass('active');
-  });
-
-//logon dropdown
-  $(".dropdown").click( function() {
-    $(this).toggleClass('active');
-  });
-  $(".sidebar-content").click(function(){
-    $(this).toggleClass('active');
-  });
-
-//overlays
-
-  $(".overlay-trigger").click( function() {
-    $('body').addClass('overlay-active');
-  });
-
-  $(".icon-remove-circle").click( function() {
-    $('body').removeClass('overlay-active');
-  });
-
-  $("body").delegate('.overlay-bg','click', function() {
-    $('body').removeClass('overlay-active');
-  });
-
-//feedback form
-  $("#web-feedback").click( function() {
-    $(this).addClass('selected').siblings().removeClass('selected');
-    $('.web-feedback').addClass('selected').siblings().removeClass('selected');
-  });
-
-  $("#service-feedback").click( function() {
-    $(this).addClass('selected').siblings().removeClass('selected');
-    $('.service-feedback').addClass('selected').siblings().removeClass('selected');
-  });
-
-
-
-// search trigger
- $('.search > input').keypress(function(e) {
-   if(e.which == 13) {
-    window.location.replace("/search-results");
-   }
-  });
-
-//child-tiles on child pages
-
-if( $('.child-tiles li.active').length > 0){
-
-  var countTiles = $(".child-tiles li.expandable-tile").length;
-
-  if(countTiles > 3){
-    $('.expandable-tile').children('.child-content').prepend($("<div class='icon-remove-circle'></div>"));
-  }
-  else if(countTiles <= 3){
-  }
-}
-
-$('.child-page .child-tile').click(function(){
-  var clickedTile = $(this).parent();
-  clickedTile.addClass('active');
-  clickedTile.siblings().removeClass('active');
-
-});
-
-
-
-
-//removing delete
-$(".expandable-tile").delegate('.icon-remove-circle','click', function() {
-  var thisParent = $(this).closest('li.expandable-tile');
-  thisParent.removeClass('active');
-});
-
-// How to choose guide
-$('.help-choose .prompt').click(function(){
-  $(this).parent().addClass('active');
-  $('.help-choose').addClass('selected');
-  $('.small-nav').click(function(){
-    $('.help-choose').removeClass('selected');
-    $('.help-choose li').removeClass('active');
-  });
-});
-
-// //fixing tables
-// $(function() {
-
-//     var tableHeader  = $(".table-header");
-//     var table = $(".compare-table");
-//     $(window).scroll(function() {
-//         if ($(window).scrollTop() > tableHeader.offset().top) {
-//             tableHeader.addClass('scrolled');
-//         }
-//         else if ($(window).scrollTop() < table.offset().top) {
-//             tableHeader.removeClass('scrolled');
-//         }
-
-//     });
-
-// });
-
-$(".blog-comments").click(function(){
-  $(this).addClass("active");
-});
-
-//calculators
-
-var interestArray = [{backtoBasics: "5.41",
-                      standardVariable: "5.99",
-                      accessEquity: "6.09",
-                      fixedRate: "5.09",
-                      homePackage: "4.94",
-                      blank: "",
-                    }]
-
-
-
-$('#product-selector').change(function(){
-
-  var selected_item = $(":selected", this).text()
-  
-  $.each(interestArray, function(index, interest){
-    if(selected_item=="Back to Basics"){
-      $("#interest-rate").val(interest.backtoBasics);
-    }
-    if(selected_item=="Standard Variable"){
-      $("#interest-rate").val(interest.standardVariable);
-    }
-    if(selected_item=="Access Equity"){
-      $("#interest-rate").val(interest.accessEquity);
-    }
-    if(selected_item=="Fixed Rate Loan"){
-      $("#interest-rate").val(interest.fixedRate);
-    }
-    if(selected_item=="Home Package Plus"){
-      $("#interest-rate").val(interest.homePackage);
-    }
-    if(selected_item=="Select a Product"){
-      $("#interest-rate").val(interest.blank);
-    }
-  });
-
-});
-
-$("#results-display-range").change(function() {
-  var val = $("#results-display-range").val();
-  var val2 = parseInt(val, 10);
-  $("#big-home-animation").removeClass().addClass("change" + val2 + "");
-});
-
-$(".header-toggle").click(function(){
-  $(".alternate-options").toggleClass("show");
-});
-
-$(".app-motto span").click(function(){
-  $(".app-motto").css("display", "none");
-});
-
-$('.toggler').click(function(){
+$('.toggler1').click(function(){
   $(this).toggleClass("togglerOn");
-  $(".something").toggleClass("hider-class");
-  $(".battery-class").toggleClass("dc-yes");
-});
-$('.batteryQuery').click(function(){
-  $(".battery-content").toggleClass("hider-class");
-});
-$('.solarQuery').click(function(){
-  $(".solar-content").toggleClass("hider-class");
-});
-$('.otherQuery').click(function(){
-  $(".other-content").toggleClass("hider-class");
-});
-$('.solarToggler').click(function(){
-    $(this).toggleClass("solarTogglerOn");
-    $(".solar-content").toggleClass("show-class");
-    $(".solar-class").toggleClass("solar-yes");
-});
-  $('.inverterToggler').click(function(){
-      $(".extra-inverter").toggleClass("hidden");
-});
-$('.batteryToggler').click(function(){
-    $(this).toggleClass("batteryTogglerOn");
-    $(".whatever").toggleClass("show-class");
-    $(".solar-class").toggleClass("solar-yes");
-});
-$('.otherToggler').click(function(){
-    $(this).toggleClass("batteryTogglerOn");
-    $(".whatever").toggleClass("show-class");
-    $(".solar-class").toggleClass("solar-yes");
+  $(".radio-content-1").toggleClass("show-class");
+  $(".radio-1").toggleClass("radio-toggle-alt");
+  $(".radio-2").toggleClass("radio-toggle-alt");
+  });
+$('.toggler2').click(function(){
+  $(this).toggleClass("togglerOn");
+  $(".radio-content-2").toggleClass("show-class");
+  $(".radio-3").toggleClass("radio-toggle-alt");
+  $(".radio-4").toggleClass("radio-toggle-alt");
 });
 $('.windQuery').click(function(){
-    $(".wind-content").toggleClass("hidden");
+  $(".wind-content").toggleClass("hidden");
 });
 $('.hydroQuery').click(function(){
-    $(".hydro-content").toggleClass("hidden");
+  $(".hydro-content").toggleClass("hidden");
+});
+$('.inverterToggler1').click(function(){
+  $(".extra-inverter1").toggleClass("hidden");
+});
+$('.inverterToggler2').click(function(){
+  $(".extra-inverter2").toggleClass("hidden");
 });
 $('.inverterSelector').change(function(){
-    if ($(this).val() === '2') {
-        $(".linkContentOne").toggleClass("hidden");
-    }
-    if ($(this).val() === '3') {
-        $(".linkContentTwo").toggleClass("hidden");
-    }
+if ($(this).val() === '2') {
+    $(".linkContent1").show();
+    $(".linkContent2").hide();
+}
+if ($(this).val() === '3') {
+    $(".linkContent2").show();
+    $(".linkContent1").hide();
+}
 });
 });
+
+
+
+// $('.batteryQuery').click(function(){
+//   $(".battery-content").toggleClass("hider-class");
+// });
+// $('.solarQuery').click(function(){
+//   $(".solar-content").toggleClass("hider-class");
+// });
+// $('.otherQuery').click(function(){
+//   $(".other-content").toggleClass("hider-class");
+// });
+// $('.solarToggler').click(function(){
+//     $(this).toggleClass("solarTogglerOn");
+//     $(".solar-content").toggleClass("show-class");
+//     $(".solar-class").toggleClass("radio-toggle");
+// });
+//   $('.inverterToggler').click(function(){
+//       $(".extra-inverter").toggleClass("hidden");
+// });
+// $('.batteryToggler').click(function(){
+//     $(this).toggleClass("batteryTogglerOn");
+//     $(".whatever").toggleClass("show-class");
+//     $(".solar-class").toggleClass("radio-toggle");
+// });
+// $('.otherToggler').click(function(){
+//     $(this).toggleClass("batteryTogglerOn");
+//     $(".whatever").toggleClass("show-class");
+//     $(".solar-class").toggleClass("radio-toggle");
+// });
+
